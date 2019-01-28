40a6fbedf67dd7179fa0ac2379ff71359e49e20b
tomwey <tomwey@163.com>
2019-01-28 09:30:16 +0800

    change config

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "YBSS后台系统"
+  config.site_title = "HHD18后台系统"
   
   config.namespace :admin do |admin|
     admin.build_menu do |menu|
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_ybss_#{Rails.env}_session"
+Rails.application.config.session_store :cookie_store, key: "_hhd_18_#{Rails.env}_session"
